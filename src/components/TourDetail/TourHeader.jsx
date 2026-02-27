51c699a3373b8c5eac09253e57de388ccd6af3d1
Meet Parmar <meetparmar4648@gmail.com>
2026-02-27 18:23:35 +0530

    Update footer and duration cards column to raw

--- src/components/TourDetail/TourHeader.jsx
+++ src/components/TourDetail/TourHeader.jsx
@@ -10,37 +10,39 @@ const TourHeader = ({ tour, displayImages }) => {
             </div>
 
             <div className="tour-detail-content-section">
-                <div className="tour-detail-header1-section">
-                    <div className="tour-detail-header1">
-                        {tour.bestPrice && <Button type="primary">Bestseller</Button>}
-                        {tour.freeCancel && <Button type="primary">Free cancellation</Button>}
-                    </div>
+                <div className="tour-detail-top-wrapper">
+                    <div className="tour-detail-header1-section">
+                        <div className="tour-detail-header1">
+                            {tour.bestPrice && <Button type="primary">Bestseller</Button>}
+                            {tour.freeCancel && <Button type="primary">Free cancellation</Button>}
+                        </div>
 
-                    <div className="tour-detail-header1-title">
-                        <p>{tour.title}</p>
-                        <div className="tour-detail-info">
-                            <p>{tour.rating || "New"}/5</p>
-                            <p>{tour.location}</p>
-                            <p>{tour.reviews || 0} reviews</p>
+                        <div className="tour-detail-header1-title">
+                            <p>{tour.title}</p>
+                            <div className="tour-detail-info">
+                                <p>{tour.rating || "New"}/5</p>
+                                <p>{tour.location}</p>
+                                <p>{tour.reviews || 0} reviews</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="tour-detail-tag">
-                    <a href="">Share</a>
-                    <a href="">wishlist</a>
+                    <div className="tour-detail-tag">
+                        <a href="">Share</a>
+                        <a href="">wishlist</a>
+                    </div>
                 </div>
 
-                <div className="tour-detail-image-wrapper">
-                    <div className="tour-detail-image-section">
-                        {displayImages.slice(0, 4).map((imgObj, index) => (
-                            <div key={index} className="tour-detail-image">
-                                <img src={imgObj.image} alt="tour-detail-image" />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="see-all-photos-btn">
-                        See all photos
-                    </div>
+                <div className="tour-detail-image-section">
+                    {displayImages.slice(0, 4).map((imgObj, index) => (
+                        <div key={index} className="tour-detail-image">
+                            <img src={imgObj.image} alt="tour-detail-image" />
+                            {index === 3 && (
+                                <div className="see-all-photos-btn">
+                                    See all photos
+                                </div>
+                            )}
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
