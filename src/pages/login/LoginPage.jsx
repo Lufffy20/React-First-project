098f7248a23a5de3b5781ffbb56739efad9c8e08
Meet Parmar <meetparmar4648@gmail.com>
2026-03-03 11:15:23 +0530

    responsive login and signup and vite js

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input, message } from 'antd';
+import { Button, Checkbox, Form, Input, message, Row, Col } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../../redux/auth/authSlice';
@@ -41,86 +41,99 @@ const App = () => {
             <div className="group-1171274837-1 d-none d-lg-block"></div>
             <div className="group-1171274837-2 d-none d-lg-block"></div>
 
-            <SideDesign />
+            <Row
+                justify="center"
+                align="middle"
+                gutter={[40, 40]} // Gap between columns
+                style={{ width: '100%', maxWidth: '1200px', margin: '0 auto', zIndex: 10 }}
+            >
+                {/* Side Design Column */}
+                <Col xs={0} sm={0} md={0} lg={12} xl={10}>
+                    <SideDesign />
+                </Col>
 
-            <div className="card">
-                <div className="content">
-                    <Form
-                        name="basic"
-                        layout="vertical"
-                        style={{ width: '100%' }}
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-
-                        <h2>Hello 👋, Welcome Back!</h2>
-                        <p>Enter your credentials and click <strong>'Sign In'</strong> to access the Annaizu Dashboard.</p>
-
-                        <Form.Item
-                            label="Email"
-                            name="email"
-                            rules={[{ required: true, message: 'Please enter your email!' }]}
-                        >
-                            <Input placeholder="Enter Email" size="large" />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please enter your password!' }]}
-                        >
-                            <Input.Password placeholder="Enter Password" size="large" />
-                        </Form.Item>
-
-                        <Form.Item>
-                            <div className="remember-forgot">
-                                <Checkbox>Remember me</Checkbox>
-                                <Link style={{ textDecoration: 'underline' }} to="/forgot-password">Forgot Password?</Link>
-                            </div>
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Button
-                                className='btn btn-primary'
-                                type="primary"
-                                htmlType="submit"
-                                size="large"
+                {/* Login Form Column */}
+                <Col xs={24} sm={24} md={18} lg={12} xl={10} style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className="card">
+                        <div className="content">
+                            <Form
+                                name="basic"
+                                layout="vertical"
                                 style={{ width: '100%' }}
+                                initialValues={{ remember: true }}
+                                onFinish={onFinish}
+                                onFinishFailed={onFinishFailed}
+                                autoComplete="off"
                             >
-                                Sign In
-                            </Button>
-                        </Form.Item>
 
-                        <Form.Item>
-                            <Button
-                                className="btn btn-small"
-                                size="large"
-                            >
-                                Office Login
-                            </Button>
-                        </Form.Item>
+                                <h2>Hello 👋, Welcome Back!</h2>
+                                <p>Enter your credentials and click <strong>'Sign In'</strong> to access the Annaizu Dashboard.</p>
 
-                        <Form.Item>
-                            <div style={{ textAlign: 'center' }}>
-                                Don't have an account? <Link to="/signup">Sign Up</Link>
-                            </div>
-                        </Form.Item>
+                                <Form.Item
+                                    label="Email"
+                                    name="email"
+                                    rules={[{ required: true, message: 'Please enter your email!' }]}
+                                >
+                                    <Input placeholder="Enter Email" size="large" />
+                                </Form.Item>
 
-                        <Form.Item>
-                            <div style={{ textAlign: 'center' }}>
-                                <Link to="/privacy-policy">Privacy Policy</Link> · <Link to="/terms-of-service">Terms of Service</Link>
-                            </div>
-                        </Form.Item>
+                                <Form.Item
+                                    label="Password"
+                                    name="password"
+                                    rules={[{ required: true, message: 'Please enter your password!' }]}
+                                >
+                                    <Input.Password placeholder="Enter Password" size="large" />
+                                </Form.Item>
 
-                        <p style={{ textAlign: 'center' }}>
-                            2025 © I Telenet Engineering Solution Ltd
-                        </p>
+                                <Form.Item>
+                                    <div className="remember-forgot">
+                                        <Checkbox>Remember me</Checkbox>
+                                        <Link style={{ textDecoration: 'underline' }} to="/forgot-password">Forgot Password?</Link>
+                                    </div>
+                                </Form.Item>
 
-                    </Form>
-                </div>
-            </div>
+                                <Form.Item>
+                                    <Button
+                                        className='btn btn-primary'
+                                        type="primary"
+                                        htmlType="submit"
+                                        size="large"
+                                        style={{ width: '100%' }}
+                                    >
+                                        Sign In
+                                    </Button>
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button
+                                        className="btn btn-small"
+                                        size="large"
+                                    >
+                                        Office Login
+                                    </Button>
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <div style={{ textAlign: 'center' }}>
+                                        Don't have an account? <Link to="/signup">Sign Up</Link>
+                                    </div>
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <div style={{ textAlign: 'center' }}>
+                                        <Link to="/privacy-policy">Privacy Policy</Link> · <Link to="/terms-of-service">Terms of Service</Link>
+                                    </div>
+                                </Form.Item>
+
+                                <p style={{ textAlign: 'center' }}>
+                                    2025 © I Telenet Engineering Solution Ltd
+                                </p>
+
+                            </Form>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
--- src/pages/signup/SignupPage.jsx
+++ src/pages/signup/SignupPage.jsx
@@ -34,111 +34,122 @@ const App = () => {
             <div className="group-1171274837-1 d-none d-lg-block"></div>
             <div className="group-1171274837-2 d-none d-lg-block"></div>
 
-            <SideDesign />
+            <Row
+                justify="center"
+                align="middle"
+                gutter={[40, 40]}
+                style={{ width: '100%', maxWidth: '1200px', margin: '0 auto', zIndex: 10 }}
+            >
+                <Col xs={0} sm={0} md={0} lg={12} xl={10}>
+                    <SideDesign />
+                </Col>
 
-            <div className="signup-card">
-                <div className="signup-content">
-                    <Form
-                        name="signup"
-                        layout="vertical"
-                        style={{ width: '100%' }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                        size="large"
-                    >
-                        <h2>Register Account</h2>
-                        <p>Create Your Annaizu Account - Trusted by Leading Employers.</p>
+                <Col xs={24} sm={24} md={18} lg={12} xl={10} style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className="signup-card">
+                        <div className="signup-content">
+                            <Form
+                                name="signup"
+                                layout="vertical"
+                                style={{ width: '100%' }}
+                                onFinish={onFinish}
+                                onFinishFailed={onFinishFailed}
+                                autoComplete="off"
+                                size="large"
+                            >
+                                <h2>Register Account</h2>
+                                <p>Create Your Annaizu Account - Trusted by Leading Employers.</p>
 
-                        <Row gutter={16}>
-                            <Col span={12}>
+                                <Row gutter={16}>
+                                    <Col span={12}>
+                                        <Form.Item
+                                            label="First Name"
+                                            name="firstName"
+                                            rules={[{ required: true, message: 'Please enter your first name!' }]}
+                                        >
+                                            <Input placeholder="Enter First Name" />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={12}>
+                                        <Form.Item
+                                            label="Last Name"
+                                            name="lastName"
+                                            rules={[{ required: true, message: 'Please enter your last name!' }]}
+                                        >
+                                            <Input placeholder="Enter Last Name" />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+
                                 <Form.Item
-                                    label="First Name"
-                                    name="firstName"
-                                    rules={[{ required: true, message: 'Please enter your first name!' }]}
+                                    label="Company Name"
+                                    name="companyName"
+                                    rules={[{ required: true, message: 'Please enter your company name!' }]}
                                 >
-                                    <Input placeholder="Enter First Name" />
+                                    <Input placeholder="Search Company" />
                                 </Form.Item>
-                            </Col>
-                            <Col span={12}>
+
                                 <Form.Item
-                                    label="Last Name"
-                                    name="lastName"
-                                    rules={[{ required: true, message: 'Please enter your last name!' }]}
+                                    label="Phone Number"
+                                    name="phoneNumber"
+                                    rules={[{ required: true, message: 'Please enter your phone number!' }]}
                                 >
-                                    <Input placeholder="Enter Last Name" />
+                                    <Input addonBefore="+91" placeholder="Enter Phone Number" />
                                 </Form.Item>
-                            </Col>
-                        </Row>
 
-                        <Form.Item
-                            label="Company Name"
-                            name="companyName"
-                            rules={[{ required: true, message: 'Please enter your company name!' }]}
-                        >
-                            <Input placeholder="Search Company" />
-                        </Form.Item>
+                                <Form.Item
+                                    label="Email"
+                                    name="email"
+                                    rules={[
+                                        { required: true, message: 'Please enter your email!' },
+                                        { type: 'email', message: 'Please enter valid email!' }
+                                    ]}
+                                >
+                                    <Input placeholder="Enter Email" />
+                                </Form.Item>
 
-                        <Form.Item
-                            label="Phone Number"
-                            name="phoneNumber"
-                            rules={[{ required: true, message: 'Please enter your phone number!' }]}
-                        >
-                            <Input addonBefore="+91" placeholder="Enter Phone Number" />
-                        </Form.Item>
+                                <Form.Item
+                                    label="Password"
+                                    name="password"
+                                    rules={[
+                                        { required: true, message: 'Please enter your password!' },
+                                        { min: 6, message: 'Password must be at least 6 characters!' }
+                                    ]}
+                                >
+                                    <Input.Password placeholder="Enter Password" />
+                                </Form.Item>
 
-                        <Form.Item
-                            label="Email"
-                            name="email"
-                            rules={[
-                                { required: true, message: 'Please enter your email!' },
-                                { type: 'email', message: 'Please enter valid email!' }
-                            ]}
-                        >
-                            <Input placeholder="Enter Email" />
-                        </Form.Item>
+                                <Form.Item
+                                    style={{ textAlign: 'left' }}
+                                    name="termsAndConditions"
+                                    valuePropName="checked"
+                                    rules={[
+                                        {
+                                            validator: (_, value) =>
+                                                value ? Promise.resolve() : Promise.reject(new Error('You must accept the terms')),
+                                        },
+                                    ]}
+                                >
+                                    <Checkbox>
+                                        I agree to <Link to="/terms-of-service"><b>Terms of Use</b></Link> and{' '}
+                                        <Link to="/privacy-policy"><b>Privacy Policy</b></Link>
+                                    </Checkbox>
+                                </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[
-                                { required: true, message: 'Please enter your password!' },
-                                { min: 6, message: 'Password must be at least 6 characters!' }
-                            ]}
-                        >
-                            <Input.Password placeholder="Enter Password" />
-                        </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" size="large" style={{ width: '100%' }}>
+                                        Sign Up
+                                    </Button>
+                                </Form.Item>
 
-                        <Form.Item
-                            style={{ textAlign: 'left' }}
-                            name="termsAndConditions"
-                            valuePropName="checked"
-                            rules={[
-                                {
-                                    validator: (_, value) =>
-                                        value ? Promise.resolve() : Promise.reject(new Error('You must accept the terms')),
-                                },
-                            ]}
-                        >
-                            <Checkbox>
-                                I agree to <Link to="/terms-of-service"><b>Terms of Use</b></Link> and{' '}
-                                <Link to="/privacy-policy"><b>Privacy Policy</b></Link>
-                            </Checkbox>
-                        </Form.Item>
+                                <div style={{ textAlign: 'center' }}>
+                                    Remembered your password? <Link to="/login"><b>Login Here</b></Link>
+                                </div>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" size="large" style={{ width: '100%' }}>
-                                Sign Up
-                            </Button>
-                        </Form.Item>
-
-                        <div style={{ textAlign: 'center' }}>
-                            Remembered your password? <Link to="/login"><b>Login Here</b></Link>
+                            </Form>
                         </div>
-
-                    </Form>
-                </div>
-            </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
